b8dea79bb358430bfe5284d97bc55c4c88b88ff3
Apex <robsonlawson3@gmail.com>
2025-04-29 21:12:19 +0100

    Match recent component system refactor

--- src/main/java/dev/apexstudios/infusedfoods/cauldron/PotionCauldronBlock.java
+++ src/main/java/dev/apexstudios/infusedfoods/cauldron/PotionCauldronBlock.java
@@ -1,8 +1,7 @@
 package dev.apexstudios.infusedfoods.cauldron;
 
-import dev.apexstudios.apexcore.lib.component.ComponentRegistrar;
 import dev.apexstudios.apexcore.lib.component.block.BaseBlockComponentHolder;
-import dev.apexstudios.apexcore.lib.component.block.BlockComponent;
+import dev.apexstudios.apexcore.lib.component.block.BlockComponentRegistrar;
 import dev.apexstudios.apexcore.lib.component.block.BlockComponentTypes;
 import dev.apexstudios.apexcore.lib.component.block.types.LayeredCauldronBlockComponent;
 import net.minecraft.core.BlockPos;
@@ -17,7 +16,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
@@ -44,7 +42,7 @@ public final class PotionCauldronBlock extends BaseBlockComponentHolder {
     }
 
     @Override
-    protected void registerComponents(ComponentRegistrar<BlockComponent, Block> registrar) {
+    protected void registerComponents(BlockComponentRegistrar registrar) {
         registrar.register(BlockComponentTypes.LAYERED_CAULDRON, builder -> builder.interactions(PotionCauldronSetup.INTERACTIONS));
     }
 
