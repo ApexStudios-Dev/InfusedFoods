7a9ca53645f5141b18f1f6c2f99f08c89d1df944
Apex <robsonlawson3@gmail.com>
2025-06-13 18:35:18 +0100

    Bump versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.5.35-beta-pr-38")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.5.16")
     }
 }
 
