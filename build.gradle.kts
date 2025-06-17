7d47f00c4ccbc67e9058d4b7a8c2105b750d97b0
Apex <29412632+ApexModder@users.noreply.github.com>
2025-06-17 17:47:11 +0100

    21.6 Update! (#22)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,11 @@ plugins {
 
 group = "dev.apexstudios"
 
-apex.neoVersion("21.5.75", "2025.06.01")
+apex.neoVersion("21.6.0-beta", "1.21.5", "2025.06.15")
 apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
-
-repositories {
-    maven("https://maven.apexstudios.dev/private")
-}
 
 dependencies {
     implementation(libs.apexcore)
