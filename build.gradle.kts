0e6fe60564f966d7ffb5e2492bf41b9e99288a77
Apex <29412632+ApexModder@users.noreply.github.com>
2025-07-08 19:44:10 +0100

    [21.5] Merge development changes (#26)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import dev.apexstudios.gradle.single.ApexSingleExtension
 
 plugins {
-    id("apex-conventions.neoforge")
-    id("apex-conventions.immaculate")
-    id("apex-conventions.maven-publishing")
+    id("apex-conventions.neoforge") version "0.1.69"
+    id("apex-conventions.immaculate") version "0.1.69"
+    id("apex-conventions.maven-publishing") version "0.1.69"
 }
 
 group = "dev.apexstudios"
@@ -13,10 +13,6 @@ apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
-
-repositories {
-    maven("https://maven.apexstudios.dev/private")
-}
 
 dependencies {
     implementation(libs.apexcore)
